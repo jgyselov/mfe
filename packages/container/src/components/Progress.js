d487e3d4ae53df52b8961e8d5cb8a33ab3da69e5
Julie <jgyselov@redhat.com>
2023-10-16 11:58:44 +0200

    container App

--- packages/container/src/components/Progress.js
+++ packages/container/src/components/Progress.js
@@ -1,20 +1,24 @@
-import React from "react";
-import { makeStyles, createStyles } from "@material-ui/core/styles";
-import LinearProgress from "@material-ui/core/LinearProgress";
+import React from 'react';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
-const useStyles = makeStyles((theme) => createStyles({
-  bar: {
-    width: "100%",
-    '& > * + *': {
-      marginTop: theme.spacing(2),
-    }
-  }
-})); 
+const useStyles = makeStyles((theme) => {
+  return createStyles({
+    bar: {
+      width: '100%',
+      '& > * + *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+  });
+});
 
 export default () => {
   const classes = useStyles();
 
-  return (<div className={classes.bar}>
-    <LinearProgress/>
-  </div>);
-}
+  return (
+    <div className={classes.bar}>
+      <LinearProgress />
+    </div>
+  );
+};
